8fa52c184b70a6f9fde77bbf70f47450a4c351c0
Nivedita Bauskar <nivedita.bauskar@lntinfotech.com>
2020-12-16 16:41:22 +0530

    commit1

---
+++ minMaxPrime/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minMaxPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = 10;
+            bool flag = false;
+            int max = 40;
+            for(int i = min; i <= max; i++)
+            {
+                flag = false;
+                for(int j = 2;j < i/2;j++)
+                {
+                    if(i%j==0)
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag == false)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
